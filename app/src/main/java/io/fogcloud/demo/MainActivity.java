5496331651bf0fc7fe948209f3421433b075006f
Kuki <kuki@365yiding.com>
2019-10-30 17:20:47 +0800

    wifi ok!

--- app/src/main/java/io/fogcloud/demo/MainActivity.java
+++ app/src/main/java/io/fogcloud/demo/MainActivity.java
@@ -1,7 +1,12 @@
 package io.fogcloud.demo;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -15,20 +20,24 @@ import android.widget.Toast;
 import org.json.JSONArray;
 
 import io.fogcloud.sdk.easylink.api.EasyLink;
-//import io.fogcloud.sdk.easylink.api.EasylinkP2P;
 import io.fogcloud.sdk.easylink.helper.EasyLinkCallBack;
 import io.fogcloud.sdk.easylink.helper.EasyLinkParams;
 import io.fogcloud.sdk.mdns.api.MDNS;
 import io.fogcloud.sdk.mdns.helper.SearchDeviceCallBack;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "---main---";
     private Context mContext;// 上下文
     private EditText log_view;
-    private int countno;
-    private  MDNS mdns;
+    private EditText psw;
+    private EditText ssid;
 
+    private int countno;
+    private MDNS mdns;
+
+    private EasyLink el;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,15 +46,17 @@ public class MainActivity extends AppCompatActivity {
 
         mContext = this;
 
-        final EasyLink el = new EasyLink(MainActivity.this);
+        el = new EasyLink(MainActivity.this);
         //final EasylinkP2P elp2p = new EasylinkP2P(mContext);
 
+        //listenwifichange();
 
         final TextView easylinktest = (TextView) findViewById(R.id.easylinktest);
 //        TextView easylinkstop = (TextView) findViewById(R.id.easylinkstop);
 
-        final EditText psw = (EditText) findViewById(R.id.psw);
-        final EditText ssid = (EditText) findViewById(R.id.ssid);
+        psw = (EditText) findViewById(R.id.psw);
+        ssid = (EditText) findViewById(R.id.ssid);
+
         if(ssid!=null) {
             ssid.setText(el.getSSID());
         }
@@ -55,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
             log_view.setText("");
         }
 
-        ssid.setText("IvyWang");
-        psw.setText("03513947");
+        ssid.setText("work_janady");
+        psw.setText("12345678");
 
         mdns = new MDNS(this);
 
@@ -79,28 +90,6 @@ public class MainActivity extends AppCompatActivity {
                             public void onSuccess(int code, String message) {
 //                                Log.d(TAG,">>>>>>>>>>");
                                 Log.d(TAG, message);
-                                mdns.startSearchDevices("_easylink._tcp.local.", new SearchDeviceCallBack() {
-                                    @Override
-                                    public void onSuccess(int code, String message) {
-                                        super.onSuccess(code, message);
-                                        Log.d("---mdns---", message);
-                                    }
-
-                                    @Override
-                                    public void onFailure(int code, String message) {
-                                        super.onFailure(code, message);
-                                        Log.d("---mdns---", message);
-                                    }
-
-                                    @Override
-                                    public void onDevicesFind(int code, JSONArray deviceStatus) {
-                                        super.onDevicesFind(code, deviceStatus);
-                                        if (!deviceStatus.equals("")) {
-                                            Log.d("---mdns---", deviceStatus.toString());
-                                        }
-                                    }
-                                });
-
                                 send2handler(1, message);
                             }
 
@@ -110,6 +99,32 @@ public class MainActivity extends AppCompatActivity {
                                 Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
                             }
                         });
+
+                        mdns.startSearchDevices("_easylink._tcp.local.", new SearchDeviceCallBack() {
+                            @Override
+                            public void onSuccess(int code, String message) {
+                                super.onSuccess(code, message);
+                                Log.d("---mdns---", "\ncode="+code+"\nmessage=\n"+message);
+                            }
+
+                            @Override
+                            public void onFailure(int code, String message) {
+                                super.onFailure(code, message);
+                                Log.d("---mdns---", "\ncode="+code+"\nmessage=\n"+message);
+                            }
+
+                            @Override
+                            public void onDevicesFind(int code, JSONArray deviceStatus) {
+                                super.onDevicesFind(code, deviceStatus);
+                                if (!deviceStatus.equals("")) {
+                                    send2handler(3, deviceStatus.toString());
+                                    Log.d("---mdns---","\ncode="+code+"\ndeviceInfo=\n"+deviceStatus.toString());
+                                }else{
+                                    Log.d("---mdns---", "\ncode="+code+"\ndeviceInfo=\n"+deviceStatus.toString());
+                                }
+                            }
+                        });
+
                     } else {
                         easylinktest.setText("发送配网");
                         easylinktest.setBackgroundColor(Color.rgb(63, 81, 181));
@@ -158,6 +173,29 @@ public class MainActivity extends AppCompatActivity {
             }
             if (msg.what == 2) {
                 log_view.setText("");
+            }
+            if (msg.what ==3 ){
+                mdns.updateMessage();
+            }
+        }
+    };
+
+    private void listenwifichange() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        this.registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                if (info.getDetailedState() == NetworkInfo.DetailedState.CONNECTED) {
+                    Log.d(TAG, "---heiheihei---");
+                    ssid.setText(el.getSSID());
+                }
             }
         }
     };
